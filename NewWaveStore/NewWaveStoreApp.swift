0656e9d30e7271830497611633e8672e55796407
AWildTeriyaki <terrancexia321@gmail.com>
2025-12-10 10:46:55 -0500

    Initial Commit

---
+++ NewWaveStore/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  NewWaveStore
+//
+//  Created by Terrance Xia (student LM) on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewWaveStoreTests/NewWaveStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreTests.swift
+//  NewWaveStoreTests
+//
+//  Created by Terrance Xia (student LM) on 12/10/25.
+//
+
+import Testing
+@testable import NewWaveStore
+
+struct NewWaveStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
